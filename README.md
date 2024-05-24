469bbfa35a929101ec94d6ab309d4c05c63f8b16
juanortiz93 <jcarlosortizalvarez@gmail.com>
2024-05-24 14:57:30 -0500

    Se agrega Test.java

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CICDPractice
 Practica de CI CD
+
+Este archivo muestra un mensaje indicando que un Commit acaba de ser realizado
---
+++ Test.java
@@ -0,0 +1,8 @@
+public class SellIntroduction  {
+
+	public static void main(String[] args) {
+		 System.out.println("A commit has been made");
+		
+	}
+
+}
